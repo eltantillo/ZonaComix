5f73ed2528c4ea26ac4f35b1cee46e566ab47ac9
eltantillo <eltantillo@gmail.com>
2015-06-05 18:19:56 -0600

    Comic Info Controller

---
+++ src/ZonaComix/WebServiceBundle/Controller/ComicInfoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ZonaComix\WebServiceBundle\Controller;
+
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ComicInfoController extends FOSRestController
+{
+    public function ComicInfoAction()
+    {
+        $comic   = $this->get('request')->request->get('Comic');
+        $chapter = $this->get('request')->request->get('Chapter');
+
+        $ChapterModel = fopen("comics/" . $comic . "/" . $chapter . "/ChapterModel.zc", "w");
+
+        $response = new Response();
+        $response->setContent("Holis");
+        $response->headers->set('Content-Type', 'text/html');
+        $response->setStatusCode(Response::HTTP_OK);
+        return $response;
+    }
+}
